dbfb93b9e3212695fcd51e2923628aa604bb3d86
flamurzenelaj <flamurzenelaj6@gmail.com>
2023-04-25 20:52:10 +0200

    Installed some dependencies and added route for home page

---
+++ user/src/routes/AppRoute.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from '../pages/HomePage'
+
+function AppRoutes() {
+  return (
+    <Fragment>
+      <Routes>
+      <Route path='/' element={<HomePage />} />
+      </Routes>
+    </Fragment>
+  )
+}
+
+export default AppRoutes
