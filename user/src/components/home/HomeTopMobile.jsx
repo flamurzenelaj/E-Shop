575dcfcf1d1a49d2696767ea7bf547d91aa3a997
jonnvokshi <jv51550@ubt-uni.net>
2023-05-04 21:02:49 +0200

    Created HomeTop component and styled it with bootstrap classes and components

---
+++ user/src/components/home/HomeTopMobile.jsx
@@ -0,0 +1,20 @@
+import React, { Component, Fragment } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+export class HomeTopMobile extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Container className="p-0 m-0 overflow-hidden" fluid={true}>
+                    <Row className="p-0 m-0 overflow-hidden">
+                        <Col lg={12} md={12} sm={12}>
+
+                        </Col>
+                    </Row>
+                </Container>
+            </Fragment>
+        )
+    }
+}
+
+export default HomeTopMobile
