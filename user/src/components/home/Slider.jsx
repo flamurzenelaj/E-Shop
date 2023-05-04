0cf2b4795d1c65c50b5d60d5990adaa126022b9b
jonnvokshi <jv51550@ubt-uni.net>
2023-05-04 21:13:47 +0200

    Created some components for future use, and added some images for different components testing...

---
+++ user/src/components/home/Slider.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Fragment } from 'react'
+
+export class Slider extends Component {
+    render() {
+        return (
+            <Fragment>
+                <h1>Slider</h1>
+            </Fragment>
+        )
+    }
+}
+
+export default Slider
