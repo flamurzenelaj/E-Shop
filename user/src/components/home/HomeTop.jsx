8b5276359b3a321caa4eff1e5a00ebda79f362b8
flamurzenelaj <flamurzenelaj6@gmail.com>
2023-05-05 15:05:03 +0200

    Created slider for Home Page and added some images to slider component

--- user/src/components/home/HomeTop.jsx
+++ user/src/components/home/HomeTop.jsx
@@ -1,26 +1,24 @@
-import React, { Component } from 'react'
-import { Fragment } from 'react'
-import { Container, Row, Col, Card } from 'react-bootstrap'
-import MegaMenu from './MegaMenu'
+import React, { Fragment} from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import HomeSlider from "./HomeSlider";
+import MegaMenu from "./MegaMenu";
 
-class HomeTop extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Container className="p-0 m-0 overflow-hidden" fluid={true}>
-                    <Row>
-                        <Col lg={3} md={3} sm={12}>
-                            <MegaMenu />
-                        </Col>
+function HomeTop() {
+  return (
+    <Fragment>
+      <Container className="p-0 m-0 overflow-hidden" fluid={true}>
+        <Row>
+          <Col lg={3} md={3} sm={12}>
+            <MegaMenu />
+          </Col>
 
-                        <Col lg={9} md={9} sm={12}>
-
-                        </Col>
-                    </Row>
-                </Container>
-            </Fragment>
-        )
-    }
+          <Col lg={9} md={9} sm={12}>
+            <HomeSlider />
+          </Col>
+        </Row>
+      </Container>
+    </Fragment>
+  );
 }
 
-export default HomeTop
+export default HomeTop;
--- user/src/pages/HomePage.jsx
+++ user/src/pages/HomePage.jsx
@@ -18,7 +18,6 @@ function HomePage() {
         <HomeTopMobile />
       </div>
 
-      <HomeTop />
     </Fragment>
   )
 }
