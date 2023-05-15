a2df9154ed9a5e608238ab211ae0efbf3d6817a0
jonnvokshi <jv51550@ubt-uni.net>
2023-05-15 17:27:42 +0200

    Created footer component for desktop and added some styling

--- user/src/pages/HomePage.jsx
+++ user/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import HomeTop from '../components/home/HomeTop'
 import NavMenuDesktop from '../components/common/NavMenuDesktop'
 import NavMenuMobile from '../components/common/NavMenuMobile'
 import HomeTopMobile from '../components/home/HomeTopMobile'
+import FooterDesktop from '../components/common/FooterDesktop'
 
 
 function HomePage() {
@@ -17,7 +18,7 @@ function HomePage() {
         <NavMenuMobile />
         <HomeTopMobile />
       </div>
-
+      <FooterDesktop />
     </Fragment>
   )
 }
