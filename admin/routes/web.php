ee240ff22fc4cc2c37a8b5d6ebc9ba8cb3c01e94
flamurzenelaj <flamurzenelaj6@gmail.com>
2023-06-08 19:27:59 +0200

    Created Admin Dashboard and updated Sign in form design

--- admin/routes/web.php
+++ admin/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +19,8 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->name('dashboard');
+
+Route::get('/logout',[AdminController::class, 'AdminLogout'])->name('admin.logout');
+
