3e158ddd4f15e0296a1b71b4755104e237fa7dd4
flamurzenelaj <flamurzenelaj6@gmail.com>
2023-06-09 17:07:28 +0200

    Updated Admin Profile Page and made it functional

--- admin/app/Http/Controllers/AdminController.php
+++ admin/app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
@@ -11,5 +12,30 @@ class AdminController extends Controller
     public function AdminLogout(Request $request){
         Auth::logout();
         return Redirect()->route('login');
+    }
+
+    public function UserProfile(){
+        $adminData = User::find(1);
+        return view('backend.admin.admin_profile', compact('adminData'));
+    }
+
+    public function UserProfileStore(Request $request){
+        $data = User::find(1);
+        $data->name = $request->name;
+        $data->email = $request->email;
+
+        if($request->file('profile_photo_path')){
+            $file = $request->file('profile_photo_path');
+            @unlink(public_path('upload/admin_images/'.$data->profile_photo_path));
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['profile_photo_path'] = $filename;
+        }
+        $data->save();
+        $notification = array(
+            'message' => 'User Profile Updated Successfully',
+            'alert_type' => 'success',
+        );
+        return redirect()->route('user.profile')-with($notification);
     }
 }
--- admin/routes/web.php
+++ admin/routes/web.php
@@ -24,3 +24,9 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 Route::get('/logout',[AdminController::class, 'AdminLogout'])->name('admin.logout');
 
+Route::prefix('admin')->group(function(){
+    Route::get('/user/profile',[AdminController::class, 'UserProfile'])->name('user.profile');
+   
+    Route::post('/user/profile/store',[AdminController::class, 'UserProfileStore'])->name('user.profile.store');
+});
+
